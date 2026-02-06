31ab1021f2de8fd21e93e46eeae0805ea04265cb
Ninditya Salma N A <ninditya.sna025@gmail.com>
2026-02-06 00:19:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8669708319-d5865",
+  "appId": "1:918997791039:web:88aab802968af72b26638a",
+  "apiKey": "AIzaSyD3Har72G6UXx45ix0il3XasC7Lwhv2Tak",
+  "authDomain": "studio-8669708319-d5865.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "918997791039"
+};
